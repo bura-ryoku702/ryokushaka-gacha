f994a6d870f3091307254dc5e3718c4f582aff12
bura_ryokumari <buranco828@gmail.com>
2025-03-05 01:04:35 +0900

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -1,30 +1,3 @@
-const items = [
-    { name: "Channel U ツアーTシャツM", price: 3800 },
-    { name: "Channel U ツアーTシャツL", price: 3800 },
-    { name: "Channel U ツアーTシャツXL", price: 3800 },
-    { name: "Channel U ツアータオル", price: 2200 },
-    { name: "Channel U ツアーアクリルキーホルダー", price: 800 },
-    { name: "Channel U ジャケ写Tシャツ(ビッグシルエット)M", price: 4200 },
-    { name: "Channel U ジャケ写Tシャツ(ビッグシルエット)XL", price: 4200 },
-    { name: "Channel U ツアーラバーバンドオーロラ", price: 600 },
-    { name: "Channel U ツアーラバーバンド黒", price: 600 },
-    { name: "ChannelU ツアーマグネット", price: 4500 },
-    { name: "PLAYER 1Tシャツ(ビッグシルエット)M", price: 4200 },
-    { name: "PLAYER 1Tシャツ(ビッグシルエット)XL", price: 4200 },
-    { name: "PLAYER 1ミトン", price: 2000 },
-    { name: "PLAYER 1巾着ポーチ", price: 1500 },
-    { name: "ブロッコリー＆カリフラワーTシャツM", price: 3800 },
-    { name: "ブロッコリー＆カリフラワーTシャツL", price: 3800 },
-    { name: "ブロッコリー＆カリフラワーTシャツXL", price: 3800 },
-    { name: "ブロッコリー＆カリフラワーブランケット", price: 3500 },
-    { name: "ブロッコリー＆カリフラワークッション", price: 3500 },
-    { name: "PLAYER 1セットアップ(ナイロン)M", price: 12000 },
-    { name: "PLAYER 1セットアップ(ナイロン)XL", price: 12000 },
-    { name: "Channel U ツアーステッカーセット", price: 900 },
-    { name: "メッシュバッグ", price: 1000 },
-    { name: "ガチャ", price: 500 }
-];
-
 document.getElementById("gacha-button").addEventListener("click", function() {
     let budget = parseInt(document.getElementById("budget").value);
     let selectedItems = [];
@@ -44,20 +17,22 @@ document.getElementById("gacha-button").addEventListener("click", function() {
         availableItems.splice(randomIndex, 1);
     }
 
-        // 結果を表示（合計金額も含む）
-    
-    document.getElementById("result").innerHTML = "ガチャ結果:<br>" + selectedItems.join("<br>") + "<br><br>合計金額: " + totalPrice + "円";
+    // 結果を表示（合計金額も含む）
+    document.getElementById("result").innerHTML = 
+        "ガチャ結果:<br>" + selectedItems.join("<br>") + 
+        "<br><br>合計金額: " + totalPrice + "円";
 
-    tweetResult(selectedItems);
+    tweetResult(selectedItems, totalPrice); // 合計金額も渡す
 });
 
-function tweetResult(menuItems) {
-    let text = "ツアーグッズ10000円ガチャの結果：\n";
+function tweetResult(menuItems, totalPrice) {
+    let text = "ツアーグッズガチャの結果：\n";
     text += menuItems.join("\n");
     text += "\n合計金額: " + totalPrice + "円";  // 合計金額を追加
-    text += "\n#ツアーグッズ10000円ガチャ";
+    text += "\n#ツアーグッズガチャ";
 
     let url = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text);
     
+    // tweet-buttonのhrefを更新してツイートリンクを作成
     document.getElementById("tweet-button").href = url;
 }
