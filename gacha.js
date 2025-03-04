282803f2eb3ae511d8de9825b449f03f41fb7fb2
bura_ryokumari <buranco828@gmail.com>
2025-03-05 01:09:55 +0900

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -8,7 +8,7 @@ const items = [
     { name: "Channel U ジャケ写Tシャツ(ビッグシルエット)XL", price: 4200 },
     { name: "Channel U ツアーラバーバンドオーロラ", price: 600 },
     { name: "Channel U ツアーラバーバンド黒", price: 600 },
-    { name: "ChannelU ツアーマグネット", price: 4500 },
+    { name: "Channel U ツアーマグネット", price: 4500 },
     { name: "PLAYER 1Tシャツ(ビッグシルエット)M", price: 4200 },
     { name: "PLAYER 1Tシャツ(ビッグシルエット)XL", price: 4200 },
     { name: "PLAYER 1ミトン", price: 2000 },
@@ -44,22 +44,20 @@ document.getElementById("gacha-button").addEventListener("click", function() {
         availableItems.splice(randomIndex, 1);
     }
 
-    // 結果を表示（合計金額も含む）
-    document.getElementById("result").innerHTML = 
-        "ガチャ結果:<br>" + selectedItems.join("<br>") + 
-        "<br><br>合計金額: " + totalPrice + "円";
+        // 結果を表示（合計金額も含む）
+    
+    document.getElementById("result").innerHTML = "ガチャ結果:<br>" + selectedItems.join("<br>") + "<br><br>合計金額: " + totalPrice + "円";
 
-    tweetResult(selectedItems, totalPrice); // 合計金額も渡す
+    tweetResult(selectedItems);
 });
 
-function tweetResult(menuItems, totalPrice) {
-    let text = "ツアーグッズガチャの結果：\n";
+function tweetResult(menuItems) {
+    let text = "ツアーグッズ10000円ガチャの結果：\n";
     text += menuItems.join("\n");
     text += "\n合計金額: " + totalPrice + "円";  // 合計金額を追加
-    text += "\n#ツアーグッズガチャ";
+    text += "\n#ツアーグッズ10000円ガチャ";
 
     let url = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text);
     
-    // tweet-buttonのhrefを更新してツイートリンクを作成
     document.getElementById("tweet-button").href = url;
 }
