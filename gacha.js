6faedbd964eb0846376ba0d1027901cb851857ec
bura_ryokumari <buranco828@gmail.com>
2025-03-05 01:07:43 +0900

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -1,3 +1,30 @@
+const items = [
+    { name: "Channel U ツアーTシャツM", price: 3800 },
+    { name: "Channel U ツアーTシャツL", price: 3800 },
+    { name: "Channel U ツアーTシャツXL", price: 3800 },
+    { name: "Channel U ツアータオル", price: 2200 },
+    { name: "Channel U ツアーアクリルキーホルダー", price: 800 },
+    { name: "Channel U ジャケ写Tシャツ(ビッグシルエット)M", price: 4200 },
+    { name: "Channel U ジャケ写Tシャツ(ビッグシルエット)XL", price: 4200 },
+    { name: "Channel U ツアーラバーバンドオーロラ", price: 600 },
+    { name: "Channel U ツアーラバーバンド黒", price: 600 },
+    { name: "ChannelU ツアーマグネット", price: 4500 },
+    { name: "PLAYER 1Tシャツ(ビッグシルエット)M", price: 4200 },
+    { name: "PLAYER 1Tシャツ(ビッグシルエット)XL", price: 4200 },
+    { name: "PLAYER 1ミトン", price: 2000 },
+    { name: "PLAYER 1巾着ポーチ", price: 1500 },
+    { name: "ブロッコリー＆カリフラワーTシャツM", price: 3800 },
+    { name: "ブロッコリー＆カリフラワーTシャツL", price: 3800 },
+    { name: "ブロッコリー＆カリフラワーTシャツXL", price: 3800 },
+    { name: "ブロッコリー＆カリフラワーブランケット", price: 3500 },
+    { name: "ブロッコリー＆カリフラワークッション", price: 3500 },
+    { name: "PLAYER 1セットアップ(ナイロン)M", price: 12000 },
+    { name: "PLAYER 1セットアップ(ナイロン)XL", price: 12000 },
+    { name: "Channel U ツアーステッカーセット", price: 900 },
+    { name: "メッシュバッグ", price: 1000 },
+    { name: "ガチャ", price: 500 }
+];
+
 document.getElementById("gacha-button").addEventListener("click", function() {
     let budget = parseInt(document.getElementById("budget").value);
     let selectedItems = [];
